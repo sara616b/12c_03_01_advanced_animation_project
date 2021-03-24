374daf1be075684d688d53cd71bf974e54db37c8
Aslak <aslakszs@hotmail.com>
2021-03-24 11:26:38 +0100

    Almost working

--- frontpage.js
+++ frontpage.js
@@ -1,26 +1,3 @@
-// const erklære en variabel med værdien burger, nav-links, nav-links li///
-const burger = document.querySelector(".burger");
-const nav = document.querySelector(".nav-links");
-const navLinks = document.querySelectorAll(".nav-links li");
-
-burger.addEventListener("click", () => {
-  //Toggle Nav
-  nav.classList.toggle("nav-active");
-
-  //Animate Links (forEach går i gennem hvert link/Element)
-  navLinks.forEach((link, index) => {
-    if (link.style.animation) {
-      //(hvis der allerede er en animation så fjernes den)
-      link.style.animation = "";
-    } else {
-      //(hvis der ikke er en animation, så tilføjes den
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.4}s`;
-    }
-  });
-  //Burger Animation (her fjernes eller påsættes toggle-klassen fra css)
-  burger.classList.toggle("toggle");
-});
-
 // PARTICLE ANIMATION
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
